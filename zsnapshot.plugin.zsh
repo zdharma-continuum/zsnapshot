56e0dba321ed7789f0fa2e5a61d55d9dde4b0ab2
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-06-09 21:22:38 +0200

    Immunity to setopt nofunctionargzero

--- zsnapshot.plugin.zsh
+++ zsnapshot.plugin.zsh
@@ -1,3 +1,8 @@
+# This gives immunity to functionargzero being unset
+# _ will be set to last argument to source builtin
+PLUGIN_UNDERSCORE="$_"
+[ "$0" != "$PLUGIN_UNDERSCORE" ] && 0="$PLUGIN_UNDERSCORE"
+
 ZSS_DIR="${0:h}"
 
 PATH="$PATH:$ZSS_DIR"
