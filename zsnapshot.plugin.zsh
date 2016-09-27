733b415d6a15e3139f33ea2415c11858b6835eb8
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-09-27 09:06:09 +0200

    snapshot -> __zsnapshot

--- zsnapshot.plugin.zsh
+++ zsnapshot.plugin.zsh
@@ -3,4 +3,4 @@ ZSS_DIR="${ZERO:h}"
 
 PATH="$PATH:$ZSS_DIR"
 
-alias zsnapshot="source snapshot > snap-\$(date +%Y.%m.%d_%H:%M:%S)"
+alias zsnapshot="source __zsnapshot > snap-\$(date +%Y.%m.%d_%H:%M:%S)"
